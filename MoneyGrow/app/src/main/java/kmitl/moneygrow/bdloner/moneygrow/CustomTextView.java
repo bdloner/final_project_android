b05321259b935ad20884a5bfbc90143726af86c5
BDLoneR <plumeria.in.sky@gmail.com>
2017-10-31 20:28:21 +0700

    Update project EP.1 splash & Intro
    update
    + splash screen
    + Intro slider
    
    -- not finish --
    
    i'll edit intro slider , It's so not good

---
+++ MoneyGrow/app/src/main/java/kmitl/moneygrow/bdloner/moneygrow/CustomTextView.java
@@ -0,0 +1,41 @@
+package kmitl.moneygrow.bdloner.moneygrow;
+
+/**
+ * Created by BDLoneR on 31/10/2560.
+ */
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.widget.TextView;
+
+public class CustomTextView extends android.support.v7.widget.AppCompatTextView {
+    public CustomTextView(Context context) {
+        super(context);
+        initTypFace();
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initTypFace();
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initTypFace();
+    }
+
+    private void initTypFace() {
+        Typeface typeface = Typeface.createFromAsset(getContext().getAssets(), "font/thaisanslite_r1.ttf");
+        setTypeface(typeface);
+        setLineSpacing(getPixelFromDp(getContext(), 10 ), 1);
+        setIncludeFontPadding(false);
+    }
+
+    public static float getPixelFromDp(Context context, float dp) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        return dp * (metrics.densityDpi / 160f);
+    }
+}
---
+++ MoneyGrow/app/src/main/java/kmitl/moneygrow/bdloner/moneygrow/SplashScreen.java
@@ -0,0 +1,32 @@
+package kmitl.moneygrow.bdloner.moneygrow;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread screenThead = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), IntroActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        screenThead.start();
+    }
+}

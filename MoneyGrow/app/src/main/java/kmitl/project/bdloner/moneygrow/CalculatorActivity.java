2b36d4b4ef33a3d0136fbca379fd9892f6e0178c
Mr.Yosapat Thamarongrak <plumeria.in.sky@gmail.com>
2017-11-03 19:38:12 +0700

    Update project Ep.2-2 Current DateTime
    not finish
    - current datetime
    
    i'm try.

--- MoneyGrow/app/src/main/java/kmitl/project/bdloner/moneygrow/CalculatorActivity.java
+++ MoneyGrow/app/src/main/java/kmitl/project/bdloner/moneygrow/CalculatorActivity.java
@@ -3,12 +3,21 @@ package kmitl.project.bdloner.moneygrow;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class CalculatorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        CustomTextView dateText = (CustomTextView) findViewById(R.id.dateText);
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+
+        dateText.setText(currentDateTimeString);
 
     }
 }
